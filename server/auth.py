36f7e920a904a2b7a60ba0a065df882efadd6123
Tejaswini S <Tejaswinis63@users.noreply.huggingface.co>
2026-04-07 12:40:10 +0000

    Create server/auth.py

---
+++ server/auth.py
@@ -0,0 +1,7 @@
+from server.database import add_user, check_user
+
+def register_user(username, password):
+    return add_user(username, password)
+
+def login_user(username, password):
+    return check_user(username, password)
